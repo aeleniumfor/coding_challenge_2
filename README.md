e86e70da40ba48124710d7970df3556c19f1f61d
sasenomura <kuzu.kamo@gmail.com>
2018-05-31 07:06:24 +0900

    完了

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-"# coding_challenge_2" 
+## Description
+Docker を用いて、 "Hello world" というメッセージ を JSON 形式で返す API 
+- http://153.122.97.129/
+
+##URL
+http://153.122.97.129/
+##### 使用したdocker image
+```docker
+golang:latest
+```
+```docker
+postgres:latest
+```
+## Usage
+
+### build and run
+```docker:
+$ git clone https://github.com/aeleniumfor/coding_challenge_1.git
+$ cd coding_challenge_1
+$ docker build -t golang_app .
+$ docker run -d -p 8080:8080 --rm golang_app
+```
+### confirmation
+
+##### ステータスコードの確認
+```
+$ curl -LI http://localhost:8080/ -o /dev/null -w '%{http_code}' -s
+```
+
+##### レスポンスの確認
+```
+$ curl -XGET -H 'Content-Type:application/json' http://localhost:8080/
+```
+
+
--- WebApp/main.go
+++ WebApp/main.go
@@ -14,27 +14,9 @@ type Hello struct {
 	Message string `json:"message"`
 }
 
-type User struct {
-	Id      string
-	Balance uint64
-}
-
 type Request struct {
 	Name  string `json:"name"`
 	Email string `json:"email"`
-}
-
-func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", handler)                                     //GET
-	r.HandleFunc("/users", users_handler).Methods("GET")           //GET
-	r.HandleFunc("/users/{id}", userid_handler).Methods("GET")     //GET
-	r.HandleFunc("/users", usercreate_handler).Methods("POST")     //POST
-	r.HandleFunc("/users/{id} ", update_handler).Methods("PUT")   //PUT
-	r.HandleFunc("/users/{id} ", delete_handler).Methods("DELETE") //DELETE
-
-	http.Handle("/", r)
-	http.ListenAndServe(":8080", nil) //ポート8080で待機
 }
 
 //ハンドラ関数定義
@@ -66,25 +48,8 @@ func users_handler(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
-	json.Indent(json_indet, json_string, "", "  ")     //jsonを整形
-	w.Header().Set("Content-Type", "application/json") //ヘッダ情報付加
-	fmt.Fprint(w, json_indet.String())
-}
-
-func userid_handler(w http.ResponseWriter, r *http.Request) {
-	println(fmt.Sprintf("%s:%s", r.Method, r.URL))
-	//r.HandleFunc("/users/{id}", userid_handler).Methods("GET") //GET
-
-	parm := mux.Vars(r)["id"] //parmの取得
-	member := p_db.DB_select_id(parm)
-	json_indet := new(bytes.Buffer)
-	json_string, err := json.Marshal(&member)
-
-	if err != nil { //err
-		log.Fatal(err)
-	}
-
-	json.Indent(json_indet, json_string, "", "  ")     //jsonを整形
+	json.Indent(json_indet, json_string, "", "  ") //jsonを整形
+	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json") //ヘッダ情報付加
 	fmt.Fprint(w, json_indet.String())
 }
@@ -104,61 +69,107 @@ func usercreate_handler(w http.ResponseWriter, r *http.Request) {
 	member := p_db.DB_select_id(id)
 
 	json_indet := new(bytes.Buffer)
-	json_string, err := json.Marshal(&member)
+	json_string, err := json.Marshal(&member[0])
 
 	if err != nil { //err
 		log.Fatal(err)
 	}
 
-	json.Indent(json_indet, json_string, "", "  ")     //jsonを整形
+	json.Indent(json_indet, json_string, "", "  ") //jsonを整形
+	w.WriteHeader(http.StatusCreated)
 	w.Header().Set("Content-Type", "application/json") //ヘッダ情報付加
 	fmt.Fprint(w, json_indet.String())
 
 	//w.Header().Set("Content-Type", "application/json") //ヘッダ情報付加
 }
 
-func update_handler(w http.ResponseWriter, r *http.Request) {
+func user_handler(w http.ResponseWriter, r *http.Request) {
 	println(fmt.Sprintf("%s:%s", r.Method, r.URL))
-
 	parm := mux.Vars(r)["id"] //get parm
-	decoder := json.NewDecoder(r.Body)
-	defer r.Body.Close()
-	var u Request
-	err := decoder.Decode(&u)
-	if err != nil {
-		log.Fatal(err)
+
+	switch r.Method {
+	case "PUT":
+		decoder := json.NewDecoder(r.Body)
+		defer r.Body.Close()
+		var u Request
+		err := decoder.Decode(&u)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if len(p_db.DB_select_id(parm)) != 0 {
+			parm = p_db.DB_update(parm, u.Name, u.Email)
+		}
+
+		member := p_db.DB_select_id(parm)
+		if len(member) == 0 {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Header().Set("Content-Type", "application/json") //ヘッダ情報付加
+			fmt.Fprint(w)
+			break
+		}
+
+		json_indet := new(bytes.Buffer)
+		json_string, err := json.Marshal(&member[0])
+
+		if err != nil { //err
+			log.Fatal(err)
+		}
+
+		json.Indent(json_indet, json_string, "", "  ") //jsonを整形
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json") //ヘッダ情報付加
+		fmt.Fprint(w, json_indet.String())
+		break
+	case "DELETE":
+
+		member := p_db.DB_select_id(parm)
+		if len(member) == 0 {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Header().Set("Content-Type", "application/json") //ヘッダ情報付加
+			fmt.Fprint(w)
+			break
+		}
+		p_db.DB_delete(parm)
+		json_indet := new(bytes.Buffer)
+		json_string, err := json.Marshal(&member[0])
+
+		if err != nil { //err
+			log.Fatal(err)
+		}
+		json.Indent(json_indet, json_string, "", "  ") //jsonを整形
+		w.WriteHeader(http.StatusNoContent)
+		w.Header().Set("Content-Type", "application/json") //ヘッダ情報付加
+		fmt.Fprint(w, json_indet.String())
+		break
+	default:
+		member := p_db.DB_select_id(parm)
+		if len(member) == 0 {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Header().Set("Content-Type", "application/json") //ヘッダ情報付加
+			fmt.Fprint(w)
+			break
+		}
+		json_indet := new(bytes.Buffer)
+		json_string, err := json.Marshal(&member[0])
+
+		if err != nil { //err
+			log.Fatal(err)
+		}
+
+		json.Indent(json_indet, json_string, "", "  ") //jsonを整形
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json") //ヘッダ情報付加
+		fmt.Fprint(w, json_indet.String())
+		break
 	}
-	id := p_db.DB_update(parm, u.Name, u.Email)
-	p_db.DB_select_id(id)
-
-	member := p_db.DB_select_id(id)
-
-	json_indet := new(bytes.Buffer)
-	json_string, err := json.Marshal(&member)
-
-	if err != nil { //err
-		log.Fatal(err)
-	}
-
-	json.Indent(json_indet, json_string, "", "  ")     //jsonを整形
-	w.Header().Set("Content-Type", "application/json") //ヘッダ情報付加
-	fmt.Fprint(w, json_indet.String())
 }
 
-func delete_handler(w http.ResponseWriter, r *http.Request) {
-	println(fmt.Sprintf("%s:%s", r.Method, r.URL))
-
-	parm := mux.Vars(r)["id"] //get parm
-	p_db.DB_delete(parm)
-	member := p_db.DB_select_id(parm)
-
-	json_indet := new(bytes.Buffer)
-	json_string, err := json.Marshal(&member)
-
-	if err != nil { //err
-		log.Fatal(err)
-	}
-	json.Indent(json_indet, json_string, "", "  ")     //jsonを整形
-	w.Header().Set("Content-Type", "application/json") //ヘッダ情報付加
-	fmt.Fprint(w, json_indet.String())
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", handler)
+	r.HandleFunc("/users/{id}", user_handler) //GET
+	r.HandleFunc("/users", users_handler).Methods("GET")
+	r.HandleFunc("/users", usercreate_handler).Methods("POST")
+	http.Handle("/", r)
+	http.ListenAndServe(":8080", nil) //ポート8080で待機
 }
